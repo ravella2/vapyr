4517eceeb8bddde452eba1ae291be1f2028cb66b
Langdon <lfroker@gmail.com>
2018-12-14 19:29:30 -0800

    added models with help from Rachele

--- vapyr/settings.py
+++ vapyr/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'vapyr_app'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,8 @@ WSGI_APPLICATION = 'vapyr.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'vapyr',
     }
 }
 
--- vapyr_app/admin.py
+++ vapyr_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import UserProfile, Game, JoinTable
 # Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(Game)
+admin.site.register(JoinTable)
