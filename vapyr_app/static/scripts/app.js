1a0ac4827e8ff7a9b13cd0046af0ddd8d37846c7
Rachele <rachele.avella@gmail.com>
2018-12-19 19:56:12 -0800

    started on new js files for logged in/logged out

--- vapyr_app/static/scripts/app.js
+++ vapyr_app/static/scripts/app.js
@@ -2,46 +2,47 @@ var results = []
 $(document).ready(function(){
 
 
-    //Populate newest games to landing page//
-    var recentGameUrl = 'https://www.giantbomb.com/api/games/?api_key=6e0060f42d81f489256e472989988c2b69e0eacc&format=jsonp&sort=original_release_date:desc&filter=original_release_date:1700-01-01|2018-12-17&limit=40'
+    // //Populate newest games to landing page//
+    // var recentGameUrl = 'https://www.giantbomb.com/api/games/?api_key=6e0060f42d81f489256e472989988c2b69e0eacc&format=jsonp&sort=original_release_date:desc&filter=original_release_date:1700-01-01|2018-12-17&limit=40'
 
-    $.ajax({
-        method: 'GET',
-        url: recentGameUrl,
-        success: handleS,
-        dataType: 'jsonp',
-        jsonp: 'json_callback',
-        crossDomain: true,
-    });
+    // $.ajax({
+    //     method: 'GET',
+    //     url: recentGameUrl,
+    //     success: handleS,
+    //     dataType: 'jsonp',
+    //     jsonp: 'json_callback',
+    //     crossDomain: true,
+    // });
 
 
-    function handleS(games){
-        $('.games').empty();
-        results = games.results;
-        games.results.forEach(result => {
-            releaseDate= result.original_release_date.split(' ')[0]
-            let card1 = `
-            <div class= "row gamerow valign-wrapper ">
-                <div class="col l3 ">
-                    <a target="_blank" href="${result.site_detail_url}"><img class="responsive-img" src="${result.image.screen_url}"></a>
-                </div>
-                <div class="col l6">
-                    <h5>${result.name}</h5><h6>Released: ${releaseDate}</h6>
-                    <p>${result.deck}</p>
-                </div>
-                <div class="col l3 valign-wrapper">
-                    <ul>
-                        <li><a class="${result.id} waves-effect waves-light center-align light-blue darken-4 btn" id='add-current'>Add to Games List</a></li>
-                        <li><a class="${result.id} waves-effect waves-light center-align light-blue darken-4 btn" id='add-wish'>Add to Wishlist</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="divider"></div>`
-            $('.games').append(card1)
+    // function handleS(games){
+    //     $('.games').empty();
+    //     results = games.results;
+    //     games.results.forEach(result => {
+    //         releaseDate= result.original_release_date.split(' ')[0]
+    //         let card1 = `
+    //         <div class= "row gamerow valign-wrapper ">
+    //             <div class="col l3 ">
+    //                 <a target="_blank" href="${result.site_detail_url}"><img class="responsive-img" src="${result.image.screen_url}"></a>
+    //             </div>
+    //             <div class="col l6">
+    //                 <h5>${result.name}</h5><h6>Released: ${releaseDate}</h6>
+    //                 <p>${result.deck}</p>
+    //             </div>
+    //             <div class="col l3 valign-wrapper">
+    //                 <ul>
+    //                     <li><a class="${result.id} waves-effect waves-light center-align light-blue darken-4 btn" id='add-current'>Add to Games List</a></li>
+    //                     <li><a class="${result.id} waves-effect waves-light center-align light-blue darken-4 btn" id='add-wish'>Add to Wishlist</a></li>
+    //                 </ul>
+    //             </div>
+    //         </div>
+    //         <div class="divider"></div>`
+    //         $('.games').append(card1)
             
-        })
-    }
+    //     })
+    // }
 
+    //Search Functionality//
     $('.search').on('submit', function(e){
         e.preventDefault();
 
